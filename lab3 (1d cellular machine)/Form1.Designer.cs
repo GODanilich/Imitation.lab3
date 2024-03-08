f986c868b8dd70f7b29a059f55033984aec58809
Danilich <danilakop03@gmail.com>
2024-03-08 22:26:08 +0700

    Initial commit

---
+++ lab3 (1d cellular machine)/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace lab3__1d_cellular_machine_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.rule = new System.Windows.Forms.TextBox();
+            this.numberOfLines = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(23)))), ((int)(((byte)(24)))));
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1020, 1020);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(1076, 99);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(216, 95);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Старт";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // rule
+            // 
+            this.rule.Location = new System.Drawing.Point(1471, 99);
+            this.rule.Name = "rule";
+            this.rule.Size = new System.Drawing.Size(100, 20);
+            this.rule.TabIndex = 2;
+            this.rule.Text = "RuleNumber";
+            // 
+            // numberOfLines
+            // 
+            this.numberOfLines.Location = new System.Drawing.Point(1471, 174);
+            this.numberOfLines.Name = "numberOfLines";
+            this.numberOfLines.Size = new System.Drawing.Size(100, 20);
+            this.numberOfLines.TabIndex = 3;
+            this.numberOfLines.Text = "NumOfLines";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::lab3__1d_cellular_machine_.Properties.Resources.background;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1765, 1090);
+            this.Controls.Add(this.numberOfLines);
+            this.Controls.Add(this.rule);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox rule;
+        private System.Windows.Forms.TextBox numberOfLines;
+    }
+}
+
